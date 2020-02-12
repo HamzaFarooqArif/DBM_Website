d1e97fad10fbcbb8deb837cc740476142b42f806
Hamza <m.hamza.arif786@gmail.com>
2020-02-12 23:01:55 +0500

    First Commit V2

---
+++ DBM_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBM_Website.Startup))]
+namespace DBM_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
