4ac4a2fa30519e3c18f4d22b9cc7305727d67be8
Hamza <m.hamza.arif786@gmail.com>
2020-02-12 22:48:11 +0500

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DBM_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
